ae943413dc89d8ff7a17b648c532cd690d0efa19
anarehder <anapaula.rehder@gmail.com>
2024-05-23 15:59:16 -0300

    fix: numero de digitos no valor contratos

--- src/pages/ContractsPage.jsx
+++ src/pages/ContractsPage.jsx
@@ -79,7 +79,7 @@ function Contracts() {
                                 {contract.VencimentoContrato}
                             </h3>
                             <h3>
-                                R$ {new Intl.NumberFormat('pt-BR').format(contract.Valor)}
+                                R$ {new Intl.NumberFormat('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}).format(contract.Valor)}
                             </h3>
                             <Link to="/client" onClick={()=>setTenancy(contract.Tenancy)}>
                                 <button>
